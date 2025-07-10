b3062d7608df9050c57eefc095feaccd6fcb3bcb
Narasimhaa25 <narasimhayalakarajula@gmail.com>
2025-07-10 21:21:32 +0530

    Commit2

---
+++ ArithmeticOperations.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperations {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+        int difference = b - a;
+        System.out.println("Difference: " + difference);
+        int product = a * b;
+        System.out.println("Product: " + product);
+        int quotient = b / a;
+        System.out.println("Quotient: " + quotient);
+       
+    }
+}
